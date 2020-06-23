60d05d2d2cfc7692c3a2dd01e4e5255642b4a799
Umer Mehmood <476061@gmail.com>
2020-06-23 15:59:39 +0500

    End of the JS Basics

---
+++ Basics/Functions.js
@@ -0,0 +1,15 @@
+
+// Fundemantal Building block of JS
+
+// Performing a task
+function greet(name) {
+    // BODY of the FUNCTION
+    console.log("Hello " + name);
+}
+
+greet('Umer');
+
+// Calaculate a value
+function sqaure(number) {
+    return number * number;
+}
---
+++ Basics/index.js
@@ -0,0 +1,4 @@
+        // Comment
+        console.log('Hello World');
+        alert("Are you an idiot!");
+
